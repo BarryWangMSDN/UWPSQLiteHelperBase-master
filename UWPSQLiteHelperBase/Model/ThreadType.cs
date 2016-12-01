1d820c1532ad2515a7a033dbe345f0444f4cabfe
v-barryw <v-barryw@DESKTOP-Q03LT98>
2016-12-01 20:08:46 +0800

    Add to database

---
+++ UWPSQLiteHelperBase/Model/ThreadType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPSQLiteHelperBase.Model
+{
+   public class ThreadType
+    {
+        private string substatus;
+
+        public string Substatus
+        {
+            get { return substatus; }
+            set { substatus = value; }
+        }
+
+    }
+}
--- UWPSQLiteHelperBase/ViewModel/ThreadDetailsVM.cs
+++ UWPSQLiteHelperBase/ViewModel/ThreadDetailsVM.cs
@@ -8,10 +8,11 @@ using UWPSQLiteHelperBase.Model;
 using UWPSQLiteHelperBase.Helper;
 using System.Windows.Input;
 using System.Diagnostics;
+using System.ComponentModel;
 
 namespace UWPSQLiteHelperBase.ViewModel
 {
-   public class ThreadDetailsVM
+   public class ThreadDetailsVM:INotifyPropertyChanged
     {
         #region Helper Class
         SqliteHelper2 helper = new SqliteHelper2();
@@ -24,10 +25,27 @@ namespace UWPSQLiteHelperBase.ViewModel
         //right panel model
         private ThreadDetailsModel textmodel = new ThreadDetailsModel();
         public ThreadDetailsModel TextModel { get { return this.textmodel; } }
+
+        private ObservableCollection<ThreadType> threadsubstatus = new ObservableCollection<ThreadType>();
+        public ObservableCollection<ThreadType> Threadsubstatus { get { return this.threadsubstatus; } }
+
+        private ThreadType testthreadtype;
+
+        public ThreadType Testthreadtype
+        {
+            get { return testthreadtype; }
+            set { testthreadtype = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Testthreadtype"));
+            }
+        }
+
         #endregion
         #region commands
         //Insert Record Command
         private ICommand insertCommand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public ICommand InsertCommand
         {
             get
@@ -36,11 +54,13 @@ namespace UWPSQLiteHelperBase.ViewModel
             } 
         }
         public void insertrecordcommand(object detail)
-        {         
-            threadsmodel.Add((ThreadDetailsModel)detail);
+        {
+            var x = ((ThreadDetailsVM)detail).TextModel;
+            x.Casetype = Testthreadtype.Substatus;
+            threadsmodel.Add(x);
             try
             {
-                helper.InsertorReplaceThreadTable((ThreadDetailsModel)detail);
+                helper.InsertorReplaceThreadTable(x);
             }
             catch(Exception ex)
             {
@@ -61,6 +81,9 @@ namespace UWPSQLiteHelperBase.ViewModel
             textmodel.ThreadURL = "https://social.msdn.microsoft.com/Forums/windowsapps/en-US/dc505f68-d120-43e3-a9e1-d7c77746d588";
             textmodel.Owner = "v-barryw";
             textmodel.ThreadTitle = "[UWP][Desktop Bridge]unplated taskbar icons in Desktop Bridge apps";
+            //ThreadStatus initialization
+            threadsubstatus.Add(new ThreadType { Substatus = "SA" });
+            threadsubstatus.Add(new ThreadType { Substatus = "EscalationFTE" });
         }
     }
 }
